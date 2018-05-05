d1daad6c8c99339cb3d57dee54edb11e42e22273
Vierageorge <joviera@gmail.com>
2018-05-05 12:25:04 -0500

    Modified templates in order to show Posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list)
     ]
